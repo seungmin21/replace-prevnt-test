d3e456c250676a99fb0a84df59083d2bdfffda48
ohseungmin <ohmin2886@gmail.com>
2024-01-05 10:09:57 +0900

    replace()의 작성법

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const root = "링크"
 const result = root.replace("바로가기")
 console.log(result)
+
+// replace(변경할 문자열, 변경될 문자열)
+
+const word = "추가"
+console.log(word) // 추가
+console.log(word.replace('추가', '제거')) // 추가 -> 제거
